5febf097810b46ff6ef14f9601b6b8b3ad070e74
R1ftzy <r1ftzyplays@gmail.com>
2026-08-07 23:26:58 +0530

    Add links to discord and college website

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ import YearFilter from "@/components/YearFilter";
 import members from "@/data/members.json";
 import type { Member } from "@/types/member";
 
+import Link from "next/link";
+import { FaDiscord } from "react-icons/fa6";
+import IIITGLogo from "../components/IIITGLogo"
+
 const allMembers: Member[] = members;
 
 export default function Home() {
@@ -29,20 +33,41 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#121826] text-[#F1F5FF]">
       <div className="mx-auto max-w-6xl px-5 py-8 sm:px-8 sm:py-12">
-        <h1 className="font-mono text-4xl font-bold tracking-tight sm:text-5xl">
-          <span>IIITG </span>
-          <span className="text-[#8BC5FF]">Index</span>
-        </h1>
+        <div className="flex items-start justify-between">
+        <div>
+          <h1 className="font-mono text-4xl font-bold tracking-tight sm:text-5xl">
+            <span>IIITG </span>
+            <span className="text-[#8BC5FF]">Index</span>
+          </h1>
 
-        <p className="mt-5 max-w-3xl font-mono text-base leading-relaxed text-[#9AA8C7] sm:text-lg">
-          a collection of student portfolios & github profiles.{" "}
-          <span className="whitespace-nowrap font-semibold text-[#8BC5FF]">
-            {filteredMembers.length === allMembers.length
-              ? `${allMembers.length} members`
-              : `Showing ${filteredMembers.length} of ${allMembers.length} members`}
-          </span>
-        </p>
+          <p className="mt-5 max-w-3xl font-mono text-base leading-relaxed text-[#9AA8C7] sm:text-lg">
+            a collection of student portfolios & github profiles.{" "}
+            <span className="whitespace-nowrap font-semibold text-[#8BC5FF]">
+              {filteredMembers.length === allMembers.length
+                ? `${allMembers.length} members`
+                : `Showing ${filteredMembers.length} of ${allMembers.length} members`}
+            </span>
+          </p>
+          </div>
+          <div className="mt-2 flex gap-4 font-mono text-sm text-[#9AA8C7]">
+            <a
+              href="https://discord.gg/FCPj699MQU"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-[#5B8CFF]"
+            >
+              <FaDiscord size={30} />
+            </a>
 
+            <a
+              href="https://iiitg.ac.in"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            <IIITGLogo className="size-7 text-[#9AA8C7] hover:text-[#5B8CFF] transition-colors" />
+            </a>
+          </div>
+          </div>
         <div className="mt-10 h-px w-full bg-[#2B3A52]" />
 
         <SearchBar
