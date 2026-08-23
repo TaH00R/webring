a3e4f8a44076428df5f7a117cb07c8fb57a2348c
TaH00R <bgmiseason002@gmail.com>
2026-08-23 07:20:46 +0530

    Added Emails for members and Google Form support

--- types/member.ts
+++ types/member.ts
@@ -1,10 +1,10 @@
 export interface Member {
+  email: string;
   name: string;
   year: number;
   github: string;
   portfolio?: string;
   leetcode?: string;
-
   techStack?: string[];
   banner?: string;
 }
